149bcbb4d9f754f0d7516173ef3894757b22e460
bo.cheng <chengbo@QXIT-PC-000106.local>
2017-02-04 11:55:52 +0800

    支持clone子mvvm

--- mvvm.js
+++ mvvm.js
@@ -6,9 +6,14 @@ define( "Mvvm" , [
     "Template" ,
     "EventBind"
 ] , function( Base , Template , EventBind ) {
-    var RegExpKey = /<\w+\s[^>|^\{]*(\{\{[^\}]*\}\})+/gi ,
-        Mvvm      = Base.extend( function( componentName , componentTemplateId , opt ) {
-            if( !this.componentHashList[ componentName ] ) {
+    var RegExpKey                  = /<\w+\s[^>|^\{]*(\{{2,3}[^\}]*\}{2,3})+/gi ,
+        diffComponentInputHashList = {} ,
+        componentHashList          = {} ,
+        componentId                = 0 ,
+        inputIndex                 = 0 ,
+        cloneLeafDomId             = 0 ,
+        Mvvm                       = Base.extend( function( componentName , componentTemplateId , opt ) {
+            if( !componentHashList[ componentName ] ) {
                 this.componentName = componentName;
                 this.componentTemplateId = componentTemplateId;
                 document.registerElement( componentName , {
@@ -21,32 +26,27 @@ define( "Mvvm" , [
                 this.diffComponentInputs( componentName , componentTemplateId );
             }
         } , {
-            implements                 : [ new Template() ] ,
-            diffComponentInputHashList : {} ,
-            componentHashList          : {} ,
-            componentId                : 0 ,
-            inputIndex                 : 0 ,
-            diffComponentInputs        : function( name , templateId ) {
+            implements          : [ new Template() ] ,
+            diffComponentInputs : function( name , templateId ) {
                 var _html ,
                     _content ,
-                    _self         = this ,
                     _inputKeyList = {};
                 if( !this.diffComponentInputs[ name ] ) {
                     _html = this.getTemplateSources().getTemplateIdList( templateId );
                     _content = _html.TemplateContent.replace( RegExpKey , function( key ) {
-                        _key = key.replace( /.*\{\{(.*)\}\}.*/ , "$1" );
+                        _key = key.replace( /.*\{{2,3}(.*)\}{2,3}.*/ , "$1" );
                         _inputKeyList[ _key ] = [];
-                        return key.replace( /<(\w+)/ , "<$1 data-mvvm-key='" + _key + "' data-mvvm-index=" + ++_self.inputIndex );
+                        return key.replace( /<(\w+)/ , "<$1 data-mvvm-key='" + _key + "' data-mvvm-index=" + ++inputIndex );
                     } );
-                    _content = _content.replace( /<[^>|^\{]*>[^>]*(\{\{[^\}]*\}\})+[^>]*<\/[^>]*>/gi , function( key ) {
+                    _content = _content.replace( /<[^>|^\{]*>[^>]*(\{{2,3}[^\}]*\}{2,3})+[^>]*<\/[^>]*>/gi , function( key ) {
                         var _html ,
                             _hasKey = false;
-                        key.replace( /\{\{([^\{|^\}]*)\}\}/g , function( k ) {
-                            var _key = k.replace( /\{\{([^\{|^\}]*)\}\}/ , "$1" );
+                        key.replace( /\{{2,3}([^\{|^\}]*)\}{2,3}/g , function( k ) {
+                            var _key = k.replace( /\{{2,3}([^\{|^\}]*)\}{2,3}/ , "$1" );
                             if( _inputKeyList[ _key ] ) {
                                 _html = key.replace( /<[^>]+>([^<]*)<\/[^>]+>/ , "$1" );
                                 _inputKeyList[ _key ].push( {
-                                    inputIndex : ++_self.inputIndex ,
+                                    inputIndex : ++inputIndex ,
                                     template   : _html ,
                                     handleBar  : Handlebars.compile( _html )
                                 } );
@@ -55,12 +55,12 @@ define( "Mvvm" , [
                             return k;
                         } );
                         if( _hasKey ) {
-                            return key.replace( /<(\w+)/ , "<$1 data-mvvm-index=" + _self.inputIndex );
+                            return key.replace( /<(\w+)/ , "<$1 data-mvvm-index=" + inputIndex );
                         } else {
                             return key;
                         }
                     } );
-                    this.diffComponentInputHashList[ name ] = {
+                    diffComponentInputHashList[ name ] = {
                         name         : templateId ,
                         html         : _html.TemplateContent ,
                         inputKeyList : _inputKeyList ,
@@ -69,34 +69,51 @@ define( "Mvvm" , [
                 }
                 return this;
             } ,
-            clone                      : function( prop ) {
+            clone               : function( prop ) {
                 var _dom = document.createElement( this.componentName );
+                _dom.dataset.mvvmId = ++componentId;
                 this.setProp( _dom , prop );
-                _dom.dataset.mvvmId = ++this.componentId;
-                this.componentHashList[ this.componentId ] = _dom;
-                _dom.__inputKeyList = this.diffComponentInputHashList[ this.componentName ].inputKeyList;
+                componentHashList[ componentId ] = _dom;
+                _dom.__inputKeyList = diffComponentInputHashList[ this.componentName ].inputKeyList;
+                this.render( _dom );
+                _dom.__selfMvvm = this;
                 return _dom;
             } ,
-            render                     : function( dom ) {
+            render              : function( dom , isLeafDom ) {
                 var _self = this ,
-                    _str;
+                    _str ,
+                    _tmpDom ,
+                    _render     = function(){
+                        _str = diffComponentInputHashList[ _self.componentName ].handleBar( dom.__prop );
+                        dom.innerHTML = _str;
+                    };
                 window.setTimeout( function() {
-                    _str = _self.diffComponentInputHashList[ _self.componentName ].handleBar( dom.__prop );
-                    dom.innerHTML = _str;
+                    if( !dom.__prop.__leafDom.parentDom ) {
+                        _render();
+                    } else if( isLeafDom ) {
+                        _tmpDom     = document.getElementById( dom.id );
+                        _tmpDom.parentNode.replaceChild( dom , _tmpDom );
+                        _render();
+                    }
                 } , 0 );
             } ,
-            setProp                    : function( dom , prop ) {
+            setProp             : function( dom , prop ) {
                 var _self = this ,
                     _t    = false;
                 dom.prop = {};
-                dom.__prop = prop;
+                dom.__prop = Object.create( prop );
+                dom.__prop.__leafDom = {};
                 for( var a in prop ) {
+                    if( prop[ a ] instanceof HTMLElement ) {
+                        dom.__prop.__leafDom[ a ] = prop[ a ];
+                        prop[ a ].__prop.__leafDom.parentDom = dom;
+                    }
                     (function( key ) {
                         dom.prop.__defineSetter__( key , function( v ) {
                             dom.__prop[ key ] = v;
                             window.clearTimeout( _t );
                             _t = window.setTimeout( function() {
-                                _self.render( dom );
+                                _self.render( dom , true );
                             } , 0 );
                         } );
                         dom.prop.__defineGetter__( key , function() {
@@ -104,10 +121,21 @@ define( "Mvvm" , [
                         } );
                     })( a );
                 }
-                this.render( dom );
                 return this;
             }
         } );
+
+    Handlebars.registerHelper( "setLeafDom" , function( dom ) {
+        if( !( dom instanceof HTMLElement ) ) {
+            return "";
+        }
+        if( dom.__prop.__leafDom.parentDom ) {
+            dom.id = "mvvm-" + +new Date + "-" + ++cloneLeafDomId + "'";
+            dom.__selfMvvm.render( dom , true );
+        }
+        return dom.outerHTML;
+    } );
+
     new EventBind( {
         "input[data-mvvm-key] , textarea[data-mvvm-key]::keyup blur" : function() {
             var _$input     = $( this ) ,
@@ -115,7 +143,7 @@ define( "Mvvm" , [
                 _id         = _$container.data( "mvvm-id" ) ,
                 _key        = this.dataset.mvvmKey ,
                 _val        = this.value ,
-                _dom        = Mvvm.prototype.componentHashList[ _id ] ,
+                _dom        = componentHashList[ _id ] ,
                 _inputList  = _dom.__inputKeyList[ _key ];
             _dom.__prop[ _key ] = _val;
             if( _inputList ) {
